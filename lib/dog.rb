54c20efd3e0a3a46dc7184205cff5a9a3f6e52e1
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-30 17:01:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,13 @@
-# Your code goes here!
 class Dog
-
-  attr_accessor :name
   
-=begin
   def name=(name)
-    @dog_name = name
+    @name = name
   end
   
   def name
-    @dog_name
+    @name
   end
-=end
-
+  
   def bark
     puts "woof!"
   end
